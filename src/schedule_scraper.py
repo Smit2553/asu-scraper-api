004d02c4b485405dfac41405bf7e605166ed0e5d
Smit Devrukhkar <smitdev2553@gmail.com>
2023-09-23 16:21:19 -0700

    Individual Class Information Scraper Done

--- src/schedule_scraper.py
+++ src/schedule_scraper.py
@@ -13,8 +13,38 @@ def get_dynamic_soup(url: str) -> BeautifulSoup:
         return soup
 
 
-url = 'https://catalog.apps.asu.edu/catalog/classes/classlist?campusOrOnlineSelection=A&catalogNbr=230&honors=F&promod=F&searchType=all&subject=CSE&term=2241'
+def extract_data(soup, class_name):
+    elements = soup.find_all('div', class_=class_name)
+    return [element.text.strip() for element in elements]
 
-soup = get_dynamic_soup(url)
 
-print(soup.prettify())
+def parse_classes():
+    url = 'https://catalog.apps.asu.edu/catalog/classes/classlist?campusOrOnlineSelection=A&catalogNbr=230&honors=F&promod=F&searchType=all&subject=CSE&term=2241'
+    soup = get_dynamic_soup(url)
+    class_name = extract_data(soup, 'course')
+    class_id = extract_data(soup, 'number')
+    instructors = extract_data(soup, 'instructor')
+    days = extract_data(soup, 'days')
+    start_times = extract_data(soup, 'start')
+    end_times = extract_data(soup, 'end')
+    locations = extract_data(soup, 'location')
+    dates = extract_data(soup, 'dates')
+    seats = extract_data(soup, 'seats')
+    seats = [seat.replace('\xa0open seats', '') for seat in seats]
+
+    class_information = []
+    for class_name,class_id , instructor, day, start_time, end_time, location, date, seat in zip(class_name, class_id, instructors, days, start_times, end_times, locations, dates, seats):
+        class_information.append({
+            'class_name': class_name,
+            'class_id': class_id,
+            'instructor': instructor,
+            'day': day,
+            'start_time': start_time,
+            'end_time': end_time,
+            'location': location,
+            'date': date,
+            'seat': seat,
+        })
+    print(class_information)
+
+parse_classes()
