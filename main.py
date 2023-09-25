7e0d7ab38c7325868409eac37cc2abf3f392c72e
Smit Devrukhkar <smitdev2553@gmail.com>
2023-09-24 19:25:11 -0700

    Working basic API

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+import src.schedule_scraper as schedule_scraper
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Hello": "World"}
 
+
+@app.get("/schedule")
+async def get_schedule(subject: str, course_number: int, term: int):
+    schedule = await schedule_scraper.parse_classes(subject, course_number, term)
+    return schedule
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "test:app",
+        host="0.0.0.0",
+        reload=False,
+        port=8002
+    )
--- src/schedule_scraper.py
+++ src/schedule_scraper.py
@@ -1,39 +1,45 @@
+import asyncio
 from bs4 import BeautifulSoup
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 
 
-def get_dynamic_soup(url: str) -> BeautifulSoup:
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto(url)
-        page.wait_for_selector("div#class-results")
-        soup = BeautifulSoup(page.content(), "html.parser")
-        browser.close()
+async def get_dynamic_soup(url: str) -> BeautifulSoup:
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
+        await page.goto(url)
+        await page.wait_for_selector("div#class-results")
+        content = await page.content()
+        soup = BeautifulSoup(content, "html.parser")
+        await browser.close()
         return soup
 
 
-def extract_data(soup, class_name):
+async def extract_data(soup, class_name):
     elements = soup.find_all('div', class_=class_name)
     return [element.text.strip() for element in elements]
 
 
-def parse_classes(subject: str, course_number: int, term: int):
+async def parse_classes(subject: str, course_number: int, term: int):
     url = f'https://catalog.apps.asu.edu/catalog/classes/classlist?campusOrOnlineSelection=A&catalogNbr={course_number}&honors=F&promod=F&searchType=all&subject={subject}&term={term}'
-    soup = get_dynamic_soup(url)
-    class_name = extract_data(soup, 'course')
-    class_id = extract_data(soup, 'number')
-    instructors = extract_data(soup, 'instructor')
-    days = extract_data(soup, 'days')
-    start_times = extract_data(soup, 'start')
-    end_times = extract_data(soup, 'end')
-    locations = extract_data(soup, 'location')
-    dates = extract_data(soup, 'dates')
-    seats = extract_data(soup, 'seats')
+    soup = await get_dynamic_soup(url)
+    class_name = await extract_data(soup, 'course')
+    class_id = await extract_data(soup, 'number')
+    instructors = await extract_data(soup, 'instructor')
+    days = await extract_data(soup, 'days')
+    start_times = await extract_data(soup, 'start')
+    end_times = await extract_data(soup, 'end')
+    locations = await extract_data(soup, 'location')
+    dates = await extract_data(soup, 'dates')
+    seats = await extract_data(soup, 'seats')
     seats = [seat.replace('\xa0open seats', '') for seat in seats]
 
     class_information = []
-    for class_name, class_id, instructor, day, start_time, end_time, location, date, seat in zip(class_name, class_id, instructors, days, start_times, end_times, locations, dates, seats):
+    for class_name, class_id, instructor, day, start_time, end_time, location, date, seat in zip(class_name, class_id,
+                                                                                                 instructors, days,
+                                                                                                 start_times, end_times,
+                                                                                                 locations, dates,
+                                                                                                 seats):
         class_information.append({
             'class_name': class_name,
             'class_id': class_id,
@@ -45,6 +51,7 @@ def parse_classes(subject: str, course_number: int, term: int):
             'date': date,
             'seat': seat,
         })
-    # remove the first element because it is the header
+
+    # Remove the first element because it is the header
     class_information.pop(0)
     return class_information
